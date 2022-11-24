2d9805734be960a2172af68dd43b5d78c7e0c2cc
Pokkalo <89689544+Pokkalo@users.noreply.github.com>
2022-11-24 18:51:48 +0800

    first commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,20 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 7,
+        text: "I don't know. It isn't bad",
+
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: "Very well"
+    },
+    {
+        id: 3,
+        rating: 10,
+        text: "It deserves"
+    },
+]
+
+export default FeedbackData;
